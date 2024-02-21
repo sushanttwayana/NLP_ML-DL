fdcf45cc083a093bf846213f182ff32a0483d867
Sushant <65543151+sushanttwayana@users.noreply.github.com>
2024-02-21 18:43:58 +0545

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,1 @@
-## stemming
-nltk.download('stopwords')
 
-for i in corpus :
-
-  words = nltk.word_tokenize(i)
-  for word in words:
-    if word not in set(stopwords.words('english')):
-      print(stemmer.stem(word))
